127e889fc7f3e26192406d6402a07ddc8e16a642
LeoColomb <leocolombaro@yahoo.fr>
2013-09-07 23:24:20 +0200

    Clean up repo

--- src/ofxWinUtilsDialog.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "ofBaseTypes.h"
-
-#include <winuser.h>
-#include <commdlg.h>
-
-//------------------------------------------------------------------------------
-	ofColor ofxColorDialog();
